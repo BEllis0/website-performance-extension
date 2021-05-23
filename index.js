305fd666ae37e44db6428ac72ae8381f367da0a9
Brandon Ellis <brandonellis.email@gmail.com>
2021-05-23 17:06:59 -0600

    added execute script to send message to background

--- index.js
+++ index.js
@@ -5,20 +5,14 @@ var ready = (callback) => {
 };
   
 ready(() => {
-    console.log('running')
+    console.log(window)
+
     // event listener for submit button
     document.getElementById("submitBtn").addEventListener('click', () => {
 
-        // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        //     chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"}, function(data) {
-        
-        //     });
-        //   });
-        console.log(document.url)
-        chrome.runtime.sendMessage({url: 'http://www.brandontellis.com'}, null, function(response) {
-            console.log('response in index.js', response)
+        chrome.tabs.executeScript({
+            code: 'chrome.runtime.sendMessage({url: window.location.href}, function(response) { console.log("response in index.js", response) });'
         });
     });
-
     
 });
