82262df9f2f6b38b6ce9982faf3d8961bd80f678
Brandon Ellis <brandonellis.email@gmail.com>
2021-05-23 17:07:19 -0600

    keeping script for future updates

--- content_script.js
+++ content_script.js
@@ -1,7 +1,8 @@
+console.log('in content script')
 function modifyDOM(docu) {
     console.log('content script in modify dom: ', docu)
     console.log(docu.url)
-    chrome.runtime.sendMessage({url: 'http://www.brandontellis.com'}, null, function(response) {
+    chrome.runtime.sendMessage({url: 'http://www.brandontellis.com'}, function(response) {
         console.log('response in index.js', response)
     });
 
