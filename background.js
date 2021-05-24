d2e872651f1cb5dd08e80e066dd8fd5597492d74
Brandon Ellis <brandonellis.email@gmail.com>
2021-05-24 13:17:10 -0600

    cleaning up logs

--- background.js
+++ background.js
@@ -1,58 +1,31 @@
-// chrome.runtime.onInstalled.addListener(function() {
-
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//         chrome.declarativeContent.onPageChanged.addRules([{
-//           conditions: [new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'developer.chrome.com'},
-//           })
-//           ],
-//               actions: [new chrome.declarativeContent.ShowPageAction()]
-//         }]);
-//       });
-// });
-
 // handle fetch request
 const fetchPageReport = (url, waitTime) => {
     return new Promise((resolve, reject) => {
-        waitTime = waitTime || 0;
-        console.log('wait time: ', waitTime)
-        setTimeout(function() {
-            fetch(`https://lighthouse-report-files.herokuapp.com/api/reports/all?url=${url}`)
-            //.then(response => response.blob())
-            .then(data => {
-                resolve(data);
-            })
-            .catch(err => {
-                console.log('Error getting Lighthouse data: ', err);
-                reject(err);
-            });
-        }, waitTime);
+        
+        fetch(`https://lighthouse-report-files.herokuapp.com/api/reports/all?url=${url}`)
+        //.then(response => response.blob())
+        .then(response => {
+            if (response.ok) {
+                return response.blob();
+            } else {
+                console.log('page performance error response: ', response)
+                reject({message: 'Error occured, please try again in a few seconds'});
+            }
+        })
+        .then(data => {
+            resolve(data);
+        })
+        .catch(err => {
+            console.log('Error getting Lighthouse data: ', err);
+            reject({message: "Error fetching data", error: err});
+        });
     });
 };
 
-chrome.runtime.onMessage.addListener(function(message, sender, onSuccess) {
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     // if message has a url
     if (message.url) {
-        console.log('on success: ', onSuccess, onSuccess('jeff'))
         fetchPageReport(message.url)
-            .then(response => {
-                if (response.ok === false) {
-                    // rerun api call
-                    console.log('response was false');
-                    fetchPageReport(message.url, 5000)
-                        .then(res => {
-                            console.log('second res after waiting', res);
-                            return res.blob();
-                        })
-                        .catch(error => {
-                            console.log('Error in second fetch request', error);
-                        });
-                } else {
-                    // return blob
-                    console.log('data from fetchpagereport: ', response);
-                    return response.blob();
-                }
-            })
             .then(data => {
                 // create  xlsx file with the blob data and download
                 const url = window.URL.createObjectURL(new Blob([data]));
@@ -61,9 +34,12 @@ chrome.runtime.onMessage.addListener(function(message, sender, onSuccess) {
                 link.setAttribute('download', `lighthouse-report-${message.url}.xlsx`);
                 document.body.appendChild(link);
                 link.click();
+                sendResponse({message: "Download successful"});
             })
             .catch(err => {
                 console.log('Error from fetchpagereport: ', err);
+                sendResponse(err);
             });
     }
+    return true;
 });
